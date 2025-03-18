3b57668d3db8d1bc49257d85bba9423e07e4165f
Jay <jay@Jays-MacBook-Air.local>
2025-03-17 22:45:39 -0500

    Feat: statspanel

--- src/main/java/edu/uca/StatsPanel.java
+++ src/main/java/edu/uca/StatsPanel.java
@@ -1,4 +1,38 @@
 package edu.uca;
 
-public class StatsPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class StatsPanel extends JPanel {
+    private final JLabel avgDanceabilityLabel;
+    private final JLabel trackCountLabel;
+
+    private final int ROWS = 2;
+    private final int COLUMNS = 1;
+
+    public StatsPanel() {
+        setLayout(new GridLayout(ROWS, COLUMNS));
+        avgDanceabilityLabel = new JLabel("Average Danceability: ");
+        trackCountLabel = new JLabel("Track Count: ");
+
+        add(avgDanceabilityLabel);
+        add(trackCountLabel);
+    }
+
+    public void updateStats(List<Track> tracks) {
+        if (tracks.isEmpty()) {
+            avgDanceabilityLabel.setText("Average Danceability: N/A");
+            trackCountLabel.setText("Track Count: 0");
+            return;
+        }
+
+        double avgDanceability = tracks.stream()
+                .mapToDouble(Track::getDanceability)
+                .average()
+                .orElse(0.0);
+
+        avgDanceabilityLabel.setText(String.format("Average Danceability: %.2f", avgDanceability));
+        trackCountLabel.setText("Track Count: " + tracks.size());
+    }
 }
